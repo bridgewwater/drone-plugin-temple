f54e791be8d11a2aa55fb71cb67ba9084045d511
sinlov <sinlovgmppt@gmail.com>
2023-02-04 20:05:11 +0800

    fix: fix file path join use error

--- plugin_test/init_test.go
+++ plugin_test/init_test.go
@@ -9,7 +9,6 @@ import (
 	"io/fs"
 	"math/rand"
 	"os"
-	"path"
 	"path/filepath"
 	"runtime"
 	"testing"
@@ -67,7 +66,7 @@ func getOrCreateTestDataFolderFullPath() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	currentTestDataFolderAbsPath = path.Join(currentFolderPath, "testdata")
+	currentTestDataFolderAbsPath = filepath.Join(currentFolderPath, "testdata")
 	if !pathExistsFast(currentTestDataFolderAbsPath) {
 		err := mkdir(currentTestDataFolderAbsPath)
 		if err != nil {
